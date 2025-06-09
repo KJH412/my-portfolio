d7f611eff0beb98bd227330e86528e8fdf9665f8
김정현 <jh441122jh@gmail.com>
2025-06-09 16:25:43 +0900

    chore: 프로젝트 생성 및 초기 설정

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -2,6 +2,15 @@ function Header() {
     return (
       <div>
         <h1>kJH's Portfolio</h1>
+        <nav>
+            <ul>
+                <li>ABOUT</li>
+                <li>SKILLS</li>
+                <li>EXPERIENCE</li>
+                <li>PROJECTS</li>
+                <li>CONTACT</li>
+            </ul>
+        </nav>
       </div>
     )
   }
